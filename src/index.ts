95928ca255e329877beb649309e32802b641a97e
Alberto Ruiz <albertzruiz1@gmail.com>
2021-02-05 13:18:12 +0100

    feat: :tada: init commit
    first commit 'cause  is the beggining of the project

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as Discord from "discord.js"
+const TOKEN : string = "ODA3MjE1MjU3NzcyNDkwNzky.YB0vyg.TyyY7SvJVjw9zJg5-e0caepVSCQ";
+const client = new Discord.Client()
+
+client.login(TOKEN);
