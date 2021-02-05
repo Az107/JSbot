0412e29a0e336987857f67bb885e532bd73a2c95
Alberto Ruiz <albertzruiz1@gmail.com>
2021-02-05 13:48:35 +0100

    test: :white_check_mark: add jest
    jest added and initialiced also engine class was created

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as Discord from "discord.js"
+import { Engine } from "./engine";
 
 
 const TOKEN : string | undefined = process.env.TOKEN;
 if (!TOKEN ) process.exit();
+
+
 const client = new Discord.Client()
 client.login(TOKEN);
