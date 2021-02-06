ec2c6083b1908ea35b34c8ef33f0342c77e92401
Alberto Ruiz <albertzruiz1@outlook.es>
2021-02-06 01:21:59 +0100

    feat: :sparkles: add discord capabilities
    added simple discord capabilities

--- build/index.js
+++ build/index.js
@@ -1,15 +1,16 @@
 "use strict";
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var Discord = __importStar(require("discord.js"));
+var discord_js_1 = require("discord.js");
 var TOKEN = process.env.TOKEN;
 if (!TOKEN)
     process.exit();
-var client = new Discord.Client();
+var client = new discord_js_1.Client();
+client.on('ready', function () {
+    console.log("Logged in as " + client.user.tag + "!");
+});
+client.on('message', function (msg) {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
 client.login(TOKEN);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import * as Discord from "discord.js"
+import {Client} from "discord.js"
 import { Engine } from "./engine";
 
 
 const TOKEN : string | undefined = process.env.TOKEN;
 if (!TOKEN ) process.exit();
+const engine : Engine = new Engine();
+
+const client = new Client()
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user!.tag}!`);
+  });
+  
+  client.on('message', msg => {
+    if (msg.content === 'ping') {
+      msg.reply('Pong!');
+    }else{
+        msg.reply(engine.Exec(msg.content));
+    }
+  });
 
 
-const client = new Discord.Client()
 client.login(TOKEN);
